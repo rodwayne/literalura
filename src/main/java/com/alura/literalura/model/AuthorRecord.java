017878845c17369eda1e72db2e5a7ec2d60c3180
rodwayne <ricardo260898@gmail.com>
2024-07-11 10:25:38 -0500

    Create book and author records

---
+++ src/main/java/com/alura/literalura/model/AuthorRecord.java
@@ -0,0 +1,12 @@
+package com.alura.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record AuthorRecord(
+    @JsonAlias("name") String name,
+    @JsonAlias("birth_year") String birthDate
+) {
+
+}
