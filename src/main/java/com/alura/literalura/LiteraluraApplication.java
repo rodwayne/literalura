48051994bf3adb250cb55b87dfa8d42868b5835d
rodwayne <ricardo260898@gmail.com>
2024-07-11 17:15:01 -0500

    Remove unused classes

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,22 +1,12 @@
 package com.alura.literalura;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.alura.literalura.main.Main;
-import com.alura.literalura.repository.AuthorRepository;
-import com.alura.literalura.repository.BookRepository;
-
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
-	@Autowired
-	private BookRepository bookRepository;
-
-	@Autowired
-	private AuthorRepository authorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
@@ -24,8 +14,6 @@ public class LiteraluraApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Main main = new Main(bookRepository, authorRepository);
-		main.menu();
 	}
 
 }
--- src/main/java/com/alura/literalura/repository/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.alura.literalura.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alura.literalura.entities.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
