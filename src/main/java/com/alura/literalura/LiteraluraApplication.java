a9a73dd760c0293c6c399e988898939b3b34a877
rodwayne <ricardo260898@gmail.com>
2024-07-11 15:58:49 -0500

    Add command line runner to application

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,13 +1,22 @@
 package com.alura.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alura.literalura.main.Main;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.menu();
 	}
 
 }
